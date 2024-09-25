f3160fe75c9ec8550742dda8db1be1bd727524e0
Hariz Hakim <harizhakim85@gmail.com>
2024-09-25 17:19:07 +0800

    wallet connect

--- app/(tabs)/connectWallet.tsx
+++ app/(tabs)/connectWallet.tsx
@@ -1,78 +1,55 @@
-import Ionicons from '@expo/vector-icons/Ionicons';
-import { StyleSheet, Image, Platform } from 'react-native';
+import React from 'react';
+import {Button, StyleSheet, View} from 'react-native';
+import {
+  WalletConnectModal,
+  useWalletConnectModal,
+} from '@walletconnect/modal-react-native';
 
-import { Collapsible } from '@/components/Collapsible';
-import { ExternalLink } from '@/components/ExternalLink';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
-import '@walletconnect/react-native-compat'
-
-import { createAppKit, defaultConfig, AppKit } from '@reown/appkit-ethers-react-native'
-import { AppKitButton } from '@reown/appkit-ethers-react-native'
-
-const projectId = 'dcc4482bd0a2041c9f7c640ed274c8a2';
-
-const metadata = {
-  name: 'AppKit RN',
-  description: 'AppKit RN Example',
-  url: 'https://reown.com/appkit',
-  icons: ['https://avatars.githubusercontent.com/u/179229932'],
+const projectId = '7c684784ed55bc73a74c1aaf288dd50f';
+const providerMetadata = {
+  name: 'ethKL',
+  description: 'YOUR_PROJECT_DESCRIPTION',
+  url: 'https://your-project-website.com/',
+  icons: ['https://your-project-logo.com/'],
   redirect: {
-    native: 'YOUR_APP_SCHEME://'
-  }
-}
+    native: 'YOUR_APP_SCHEME://',
+    universal: 'YOUR_APP_UNIVERSAL_LINK.com',
+  },
+};
 
-const config = defaultConfig({ metadata })
+function App() {
+  const {address, open, isConnected, provider} = useWalletConnectModal();
 
-// 3. Define your chains
-const mainnet = {
-  chainId: 1,
-  name: 'Ethereum',
-  currency: 'ETH',
-  explorerUrl: 'https://etherscan.io',
-  rpcUrl: 'https://cloudflare-eth.com'
-}
+  const handleConnection = () => {
+    if (isConnected) {
+      return provider?.disconnect();
+    }
 
-const polygon = {
-  chainId: 137,
-  name: 'Polygon',
-  currency: 'MATIC',
-  explorerUrl: 'https://polygonscan.com',
-  rpcUrl: 'https://polygon-rpc.com'
-}
-
-const chains = [mainnet, polygon]
-
-createAppKit({
-  projectId,
-  chains,
-  config,
-  enableAnalytics: true // Optional - defaults to your Cloud configuration
-})
-
-export default function ConnectWallet() {
+    return open();
+  };
+  
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#D0D0D0', dark: '#353636' }}
-      headerImage={<Ionicons size={310} name="code-slash" style={styles.headerImage} />}>
-      <ThemedView>
-        <AppKit />
-        <AppKitButton />
-      </ThemedView>    
-      </ParallaxScrollView>
+    <View style={styles.container}>
+      <Button
+        onPress={handleConnection}
+        title={isConnected ? 'Disconnect' : 'Connect'}
+      />
+
+      <WalletConnectModal
+        projectId={projectId}
+        providerMetadata={providerMetadata}
+      />
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
-  headerImage: {
-    color: '#808080',
-    bottom: -90,
-    left: -35,
-    position: 'absolute',
-  },
-  titleContainer: {
-    flexDirection: 'row',
-    gap: 8,
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    backgroundColor: 'white',
+    justifyContent: 'center',
   },
 });
+
+export default App;
